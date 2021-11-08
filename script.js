e2befe1467cc6fb7d97e682c20f60b2d52be2ceb
Owl-Grey <bruss.serials@gmail.com>
2021-11-09 00:49:10 +0400

    Проверки, изменение кнопки, исправление косяков при верстке

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+
 let name_check = false
 let price_check = false
 let link_check = false
-
+let btn_check = false
 
 let name = document.getElementById('name');
 name.addEventListener('input', () => {
   if(name.value==""){
     name.style.border="1px solid #FF8484"
     document.getElementById('name_undertext').style.visibility="visible"
+    link_check = false
+    check(name_check,price_check,link_check)
   }
   else{
     name.style.border="1px"
     document.getElementById('name_undertext').style.visibility="hidden"
     name_check = true
+    check(name_check,price_check,link_check)
   }
 });
 
@@ -21,11 +25,14 @@ price.addEventListener('input', () => {
   if(price.value==""){
     price.style.border="1px solid #FF8484"
     document.getElementById('price_undertext').style.visibility="visible"
+    link_check = false
+    check(name_check,price_check,link_check)
   }
   else{
     price.style.border="1px"
     document.getElementById('price_undertext').style.visibility="hidden"
     price_check = true
+    check(name_check,price_check,link_check)
   }
 });
 
@@ -34,16 +41,31 @@ link.addEventListener('input', () => {
   if(link.value==""){
     link.style.border="1px solid #FF8484"
     document.getElementById('link_undertext').style.visibility="visible"
+    link_check = false
+    check(name_check,price_check,link_check)
   }
   else{
     link.style.border="1px"
     document.getElementById('link_undertext').style.visibility="hidden"
-    let link_check = true
-    console.log(123)
+    link_check = true
+    check(name_check,price_check,link_check)
   }
 });
 
 
-if (link_check == true && name_check == true && price_check == true){
-  console.log(1)
+function check(a,b,c){
+  if (a==true && b == true & c==true){
+    btn_check = true
+  }
+  else {btn_check = false};
+  button(btn_check)
+}
+
+
+
+function button(a){
+  if (a==true){
+    document.getElementById('button').classList.add('button_active');
+  }
+  else {document.getElementById('button').classList.remove('button_active');}
 }
